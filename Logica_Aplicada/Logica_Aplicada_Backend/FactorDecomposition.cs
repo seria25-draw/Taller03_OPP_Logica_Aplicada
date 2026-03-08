ee05717579af40c1ddd063efad33da5d61b6eef1
seria <seria@SERIANNY>
2026-03-08 15:28:39 -0500

    Taller 03 arreglado y terminado

---
+++ Logica_Aplicada/Logica_Aplicada_Backend/FactorDecomposition.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica_Aplicada_Backend;
+
+public class FactorDecomposition
+{
+    // campo
+    private int _number;
+
+    // constructor
+    public FactorDecomposition(int number)
+    {
+        Number = number;
+    }
+
+    // propiedad
+    public int Number
+    {
+        get => _number;
+        set => _number = ValidateNumber(value);
+    }
+
+    // metodo para descomponer
+    public List<int> GetFactors()
+    {
+        List<int> factores = new List<int>();
+        int n = _number;
+        int divisor = 2;
+
+        while (n > 1)
+        {
+            while (n % divisor == 0)
+            {
+                factores.Add(divisor);
+                n /= divisor;
+            }
+            divisor++;
+        }
+
+        return factores;
+    }
+
+    public string ShowFactors()
+    {
+        List<int> factores = GetFactors();
+        string resultado = string.Join(" x ", factores);
+
+        return $"{_number} = {resultado}";
+    }
+    private int ValidateNumber(int number)
+    {
+        if (number <= 1)
+        {
+            throw new ArgumentException($"El número {number} no es válido.");
+        }
+
+        return number;
+    }
+}
---
+++ Logica_Aplicada/Logica_Aplicada_Backend/MatrixOperations.cs
@@ -0,0 +1,122 @@
+﻿namespace Logica_Aplicada_Backend;
+
+public class MatrixOperations
+{
+    // campos
+    private int[,] _matrix;
+    private int _n;
+
+    // constructores
+    public MatrixOperations()
+    {
+        _n = 1;
+        _matrix = new int[_n, _n];
+        FillMatrix();
+    }
+
+    // constructor con parámetro
+    public MatrixOperations(int n)
+    {
+        _n = ValidateN(n);
+        _matrix = new int[_n, _n];
+        FillMatrix();
+    }
+
+    // propiedades
+    public int N
+    {
+        get => _n;
+        set
+        {
+            _n = ValidateN(value);
+            _matrix = new int[_n, _n];
+            FillMatrix();
+        }
+    }
+
+    // métodos
+    public void ShowMatrix()
+    {
+        for (int i = 0; i < _n; i++)
+        {
+            for (int j = 0; j < _n; j++)
+            {
+                Console.Write($"{_matrix[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public int GetSum()
+    {
+        int sum = 0;
+
+        for (int i = 0; i < _n; i++)
+        {
+            for (int j = 0; j < _n; j++)
+            {
+                sum += _matrix[i, j];
+            }
+        }
+
+        return sum;
+    }
+
+    public int GetMax()
+    {
+        int max = _matrix[0, 0];
+
+        for (int i = 0; i < _n; i++)
+        {
+            for (int j = 0; j < _n; j++)
+            {
+                if (_matrix[i, j] > max)
+                {
+                    max = _matrix[i, j];
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public int GetMin()
+    {
+        int min = _matrix[0, 0];
+
+        for (int i = 0; i < _n; i++)
+        {
+            for (int j = 0; j < _n; j++)
+            {
+                if (_matrix[i, j] < min)
+                {
+                    min = _matrix[i, j];
+                }
+            }
+        }
+
+        return min;
+    }
+
+    // métodos privados
+    private void FillMatrix()
+    {
+        for (int i = 0; i < _n; i++)
+        {
+            for (int j = 0; j < _n; j++)
+            {
+                _matrix[i, j] = (i + 1) - j;
+            }
+        }
+    }
+
+    private int ValidateN(int n)
+    {
+        if (n <= 0)
+        {
+            throw new ArgumentException($"El orden de la matriz: {n}, no es válido.");
+        }
+
+        return n;
+    }
+}
