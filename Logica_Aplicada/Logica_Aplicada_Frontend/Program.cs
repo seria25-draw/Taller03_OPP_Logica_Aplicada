cbe099d581eb8128d9ce034c046f0938884d4b19
seria <seria@SERIANNY>
2026-03-08 16:15:39 -0500

    Taller 03 terminando solo le agregue el classdiagram

--- Logica_Aplicada/Logica_Aplicada_Frontend/Program.cs
+++ Logica_Aplicada/Logica_Aplicada_Frontend/Program.cs
@@ -1,34 +1,36 @@
 ﻿using Logica_Aplicada_Backend;
 
-//Console.Write("Ingrese orden de la matriz: ");
+Console.Write("Ingrese orden de la matriz: ");
 
-//int n;
-//string? input;
+int n;
+string? input;
 
-//while (true)
-//{
-//    input = Console.ReadLine();
+while (true)
+{
+    input = Console.ReadLine();
 
-//    if (input is null)
-//    {
-//        Console.Write("No input detected. Please enter a number: ");
-//        continue;
-//    }
+    if (input is null)
+    {
+        Console.Write("No input detected. Please enter a number: ");
+        continue;
+    }
 
-//    if (int.TryParse(input, out n))
-//        break;
+    if (int.TryParse(input, out n))
+        break;
 
-//    Console.Write("Entrada inválida. Ingrese un número entero: ");
-//}
+    Console.Write("Entrada inválida. Ingrese un número entero: ");
+}
 
-//var matrix = new MatrixOperations(n);
+var matrix = new MatrixOperations(n);
 
-//Console.WriteLine("\nMatriz generada:\n");
-//matrix.ShowMatrix();
+Console.WriteLine("\nMatriz generada:\n");
+matrix.ShowMatrix();
 
-//Console.WriteLine("\nLa sumatoria es: " + matrix.GetSum());
-//Console.WriteLine("El valor máximo es: " + matrix.GetMax());
-//Console.WriteLine("El valor mínimo es: " + matrix.GetMin());
+Console.WriteLine("\nLa sumatoria es: " + matrix.GetSum());
+Console.WriteLine("El valor máximo es: " + matrix.GetMax());
+Console.WriteLine("El valor mínimo es: " + matrix.GetMin());
+
+//factores descomposicion
 
 Console.WriteLine("### Descomposición en Factores ###");
 
